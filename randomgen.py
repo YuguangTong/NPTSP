4e59e0f707257f2f28c7755392c443124d1ec2d3
Nikhil Rajpal <nikrdc@gmail.com>
2015-05-01 16:13:29 -0700

    added new gen file based on whether nodes are the same color, updated syntax in previous gen file

--- randomgen.py
+++ randomgen.py
@@ -3,35 +3,35 @@ import numpy as np
 import sys
 
 if len(sys.argv) != 3:
-    print("usage: python3 in_gen.py [n] [filename]")
+    print("usage: python3 in_gen.py [number of cities] [filename]")
     sys.exit(0)
 elif int(sys.argv[1])%2 == 1:
-    print("%d should be a constant", n)
+    print("The first argument should be a constant divisible by 2")
     sys.exit(0)
 else:
-    n = int(sys.argv[1])
+    num_cities = int(sys.argv[1])
     filename = sys.argv[2]
     myFile = open(filename, 'w')
-    arr = np.zeros((n, n), int)
+    arr = np.zeros((num_cities, num_cities), int)
     redCounter = 0
     blueCounter = 0
     color = ''
-    for i in range(n):
+    for i in range(num_cities):
         for j in range(i):
             arr[i, j] = randint(0, 100)
             arr[j, i] = arr[i, j]
-        if (randint(0, 1) == 0 and redCounter < n/2) \
-                or blueCounter == n/2:
+        if (randint(0, 1) == 0 and redCounter < num_cities/2) \
+                or blueCounter == num_cities/2:
             color +='R'
             redCounter += 1
         else:
             color += 'B'
             blueCounter += 1
 
-    myFile.write(str(n) + '\n')
-    for i in range(n):
+    myFile.write(str(num_cities) + '\n')
+    for i in range(num_cities):
         line = ''
-        for j in range(n):
+        for j in range(num_cities):
             line += str(arr[i, j]) + ' '
         myFile.write(line + '\n')
     myFile.write(color + '\n')
