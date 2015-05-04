8604e47d492d2200505b59d394ed8245d7d6c2ed
Nikhil Rajpal <nikrdc@gmail.com>
2015-05-04 12:08:53 -0700

    seemingly working version of greedy and dynamic algrorithms implemented

--- dynamo.py
+++ dynamo.py
@@ -1,7 +1,6 @@
 from itertools import combinations
 
 T = 1 # number of test cases
-fout = open ("answer.out", "w")
 for t in xrange(1, T+1):
     fin = open(str(t) + ".in", "r")
     N = int(fin.readline())
@@ -9,7 +8,6 @@ for t in xrange(1, T+1):
     for i in xrange(N):
         d[i] = [int(x) for x in fin.readline().split()]
     c = fin.readline()
-fout.close()
 
 num_cities = N
 distances = d
@@ -23,58 +21,124 @@ def cci(city):
 
 i_zero = cci(0)
 cities = range(num_cities)
+o_cities = cities + [51]
+cities.append(0)
+frozen_cities = frozenset(cities)
 C = {(frozenset([0]), 0, i_zero): 0}
-s = 2
-
-while s <= num_cities:
+s = 3
+while s <= num_cities + 1:
     subsets = list(combinations(cities, s))
     for salami in subsets:
-        if 0 in salami:
-            pastrami = frozenset(salami)
-            C[(pastrami, 0, i_zero)] = float('inf')
+        salami = list(salami)
+        if salami[0] == 0 and salami[-1] == 0:
+            del salami[-1]
+            pastrami = frozenset(salami) 
             for j in salami:
                 if j != 0:
-                    l_subset = list(salami)
+                    l_subset = salami[:]
                     l_subset.remove(j)
                     f_subset = frozenset(l_subset)
                     top = float('inf')
                     prev_color = None
                     cur_color = cci(j)
                     for v in f_subset:
-                        indicator = cci(v)
                         for i in [-3, -2, -1, 1, 2, 3]:
-                            if (f_subset, v, i) in C and indicator + i not in [-4, 4]:
+                            if (f_subset, v, i) in C and cur_color + i not in [-4, 4]:
                                 comparison = C[(f_subset, v, i)] + distances[v][j]
-                                if top >= comparison:
+                                if top > comparison:
                                     top = comparison
                                     prev_color = i
-                    if (cur_color < 0 and prev_color < 0) or (cur_color > 0 and prev_color > 0):
-                        C[(pastrami, j, prev_color+cur_color)] = top
-                    else:
-                        C[(pastrami, j, cur_color)] = top
-
+                    if prev_color:
+                        if cur_color * prev_color > 0:
+                            C[(pastrami, j, prev_color + cur_color)] = top
+                        else:
+                            C[(pastrami, j, cur_color)] = top
+            salami.append(51)
+        if salami == o_cities:
+            pastrami = frozenset(salami)
+            j = 0
+            l_subset = salami[:]
+            l_subset.remove(51)
+            f_subset = frozenset(l_subset)
+            l_subset.remove(j)
+            top = float('inf')
+            prev_color = None
+            cur_color = cci(j)
+            for v in l_subset:
+                for i in [-3, -2, -1, 1, 2, 3]:
+                    if (f_subset, v, i) in C and cur_color + i not in [-4, 4]:
+                        comparison = C[(f_subset, v, i)] + distances[v][j]
+                        if top > comparison:
+                            top = comparison
+                            prev_color = i
+            if prev_color:
+                if cur_color * prev_color > 0:
+                    C[(pastrami, j, prev_color + cur_color)] = top
+                else:
+                    C[(pastrami, j, cur_color)] = top
     s += 1
 
-rondo = []
-while cities:
+
+rondo = [0]
+s_cities = frozenset(cities + [51])
+top = float('inf')
+next_color = 0
+for i in [-3, -2, -1, 1, 2, 3]:
+    if (s_cities, 0, i) in C:
+        comparison = C[(s_cities, 0, i)]
+        if top >= comparison:
+            top = comparison
+            next_color = i
+roc = top
+prev = 0
+cities = range(num_cities)
+q = 0
+dirk = len(cities)
+while q < dirk:
     s_cities = frozenset(cities)
     top = float('inf')
-    dop = 0
+    nop = 0
+    cor = 0
+    vrep = 0
+    if next_color > 1:
+        io = [next_color - 1]
+    elif next_color == 1:
+        io = [-3, -2, -1]
+    elif next_color < -1:
+        io = [next_color + 1]
+    elif next_color == -1:
+        io = [3, 2, 1]
+    else:
+        print 'color error'
+    if cities == [0]:
+        end = None
+    else:
+        end = 0
     for j in cities:
-        if j != 0:
-            for i in [-3, -2, -1, 1, 2, 3]:
-                if (s_cities, j, i) in C and i_zero + i not in [-4, 4]:
-                    comparison = C[(s_cities, j, i)] + distances[j][0]
-                    if top >= comparison:
-                        top = comparison
-                        dop = j
-    rondo.append(dop)
-    cities.remove(dop)
-wat = distances[rondo[0]][0]
+        if j != None:
+            for i in io: 
+                if (s_cities, j, i) in C:
+                    if C[(s_cities, j, i)] + distances[j][prev] == roc:
+                        vrep = j
+                        nop = i
+                        cor = C[(s_cities, j, i)]
+    roc = cor
+    prev = vrep
+    rondo.append(prev)
+    cities.remove(prev)
+    next_color = nop
+    q += 1
+
+wat = 0
 for i in range(len(rondo)-1):
+    print rondo[i], '->', rondo[i+1], ':', distances[rondo[i]][rondo[i+1]]
     wat += distances[rondo[i]][rondo[i+1]]
-print 'OPTIMUM TOUR:'
+
+donda = []
+for r in rondo:
+    donda.append(colors[r])
+
+print 'Dynamic Programming Algorithm'
 print 'Order:', rondo
+print 'Colors:', donda
 print 'Length:', wat
-
-
---
+++ greedy.py
@@ -0,0 +1,57 @@
+T = 1 # number of test cases
+for t in xrange(1, T+1):
+    fin = open(str(t) + ".in", "r")
+    N = int(fin.readline())
+    d = [[] for i in range(N)]
+    for i in xrange(N):
+        d[i] = [int(x) for x in fin.readline().split()]
+    c = fin.readline()
+
+num_cities = N
+distances = d
+colors = c
+
+def cci(city):
+    if colors[city] == 'R':
+        return -1
+    else:
+        return 1
+
+cur_city = 0
+cities = range(num_cities)
+cities.remove(0)
+path = [0]
+at = cci(cur_city)
+while cities:
+    top = float('inf')
+    bop = 0
+    for next_city in cities:
+        if at + cci(next_city) not in [-4, 4]: 
+            comparison = distances[cur_city][next_city]
+            if top > comparison:
+                top = comparison
+                bop = next_city
+    cur_city = bop
+    if at * cci(bop) > 0:
+        at += cci(bop)
+    else:
+        at = cci(bop)
+    path.append(cur_city)
+    cities.remove(cur_city)
+
+path.reverse()
+path = [0] + path
+
+wat = 0
+for i in range(len(path)-1):
+    print path[i], '->', path[i+1], ':', distances[path[i]][path[i+1]]
+    wat += distances[path[i]][path[i+1]]
+
+donda = []
+for r in path:
+    donda.append(colors[r])
+
+print 'Greedy Algorithm'
+print 'Order:', path
+print 'Colors:', donda
+print 'Length:', wat
