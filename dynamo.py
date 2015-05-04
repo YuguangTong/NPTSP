a3fe9708f4a018cd966cea40b23a67b5b2ebc488
Nikhil Rajpal <nikrdc@gmail.com>
2015-05-04 13:40:13 -0700

    changed dynamo to start at any node.

--- dynamo.py
+++ dynamo.py
@@ -19,38 +19,36 @@ def cci(city):
     else:
         return 1
 
-i_zero = cci(0)
+C={}
 cities = range(num_cities)
-cities.append(0)
-frozen_cities = frozenset(cities)
-C = {(frozenset([0]), 0, i_zero): 0}
-s = 3
+for city in cities:
+    C[(frozenset([city]), city, cci(city))] = 0
+
+s = 2
 while s <= num_cities:
     subsets = list(combinations(cities, s))
     for salami in subsets:
         salami = list(salami)
-        if 0 in salami:
-            pastrami = frozenset(salami) 
-            for j in salami:
-                if j != 0:
-                    l_subset = salami[:]
-                    l_subset.remove(j)
-                    f_subset = frozenset(l_subset)
-                    top = float('inf')
-                    prev_color = None
-                    cur_color = cci(j)
-                    for v in f_subset:
-                        for i in [-3, -2, -1, 1, 2, 3]:
-                            if (f_subset, v, i) in C and cur_color + i not in [-4, 4]:
-                                comparison = C[(f_subset, v, i)] + distances[v][j]
-                                if top > comparison:
-                                    top = comparison
-                                    prev_color = i
-                    if prev_color:
-                        if cur_color * prev_color > 0:
-                            C[(pastrami, j, prev_color + cur_color)] = top
-                        else:
-                            C[(pastrami, j, cur_color)] = top 
+        pastrami = frozenset(salami) 
+        for j in salami:
+            l_subset = salami[:]
+            l_subset.remove(j)
+            f_subset = frozenset(l_subset)
+            top = float('inf')
+            prev_color = None
+            cur_color = cci(j)
+            for v in f_subset:
+                for i in [-3, -2, -1, 1, 2, 3]:
+                    if (f_subset, v, i) in C and cur_color + i not in [-4, 4]:
+                        comparison = C[(f_subset, v, i)] + distances[v][j]
+                        if top > comparison:
+                            top = comparison
+                            prev_color = i
+            if prev_color:
+                if cur_color * prev_color > 0:
+                    C[(pastrami, j, prev_color + cur_color)] = top
+                else:
+                    C[(pastrami, j, cur_color)] = top 
     s += 1
 
 
@@ -60,14 +58,13 @@ roc = float('inf')
 next_color = None
 prev = None
 for j in cities:
-    if j != 0:
-        for i in [-3, -2, -1, 1, 2, 3]:
-            if (s_cities, j, i) in C:
-                comparison = C[(s_cities, j, i)]
-                if roc >= comparison:
-                    roc = comparison
-                    next_color = i
-                    prev = j
+    for i in [-3, -2, -1, 1, 2, 3]:
+        if (s_cities, j, i) in C:
+            comparison = C[(s_cities, j, i)]
+            if roc >= comparison:
+                roc = comparison
+                next_color = i
+                prev = j
 rondo.append(prev)
 cities.remove(prev)
 dirk = len(cities)
