8b3f62c3d37c9e3650810472175ad3d90b9b3f47
Yuguang Tong <ygtong@berkeley.edu>
2015-05-04 22:22:33 -0700

    tidy up new_scorer_multiple.py

---
+++ compare_dp_nn.py
@@ -0,0 +1,85 @@
+import os, sys, traceback
+from nntsp.nngraph import nnGraph
+#from antTSP.antcolony import AntColony
+#from antTSP.antgraph import AntGraph
+from dptsp.dptsp import dp
+num_input = 495
+if __name__ == "__main__":
+    if len(sys.argv) == 3 and sys.argv[2]:
+        num_of_comparison = int(sys.argv[2])
+        input_dir = sys.argv[1]
+    else:
+        print("usage: python compare_dp_nn.py [instance_dir] [num_of_input_files]")
+        sys.exit(1)
+    # assume that input is in directory e.g. ./testin/
+    # output answer to ./anwswer.out
+    #fout = open("answer.out", "w")
+    os.chdir(os.path.expanduser(input_dir))
+    counter = 0
+    for t in range(1, num_input+1):
+        if counter >= num_of_comparison:
+            exit(0)
+        fin = open(str(t) + ".in", "r")
+        numCity = int(fin.readline())
+        if numCity <= 20:
+            counter += 1
+        else:
+            continue
+        distMatr = [[] for i in range(numCity)]
+        for i in range(numCity):
+            distMatr[i] = [int(x) for x in fin.readline().split()]
+        colorList = fin.readline().strip() #strip is necessary to remove space
+        
+        print '\n'
+        print "working on " + str(t) + ".in.............................."
+        print "problem size: ", numCity
+        print '\n'
+###########################################
+## Nearest neighbor
+###########################################
+        g = nnGraph(distMatr, colorList, numCity)
+        tour = g.nn_best_reversed()[0]
+        assign = [c + 1 for c in tour]
+        #fout.write("%s\n"% " ".join(map(str, assign)))
+        print "-----------NN Result-------------"
+        print "without revsersal:", assign
+        print "color:",''.join(g._colorList[i] for i in tour)
+        print "tour cost is", g.tour_cost(tour)
+###########################################                                    
+## Dynamic programming
+########################################### 
+        
+        dp(numCity, distMatr, colorList)
+
+###########################################
+## Ant colony
+###########################################
+        """
+        num_nodes = 10
+        num_ants = 30
+        num_iterations = 10
+        num_repetitions = 1
+        try:
+            graph = AntGraph(numCity, distMatr)
+            best_path_vec = None
+            best_path_cost = sys.maxsize
+            for i in range(0, num_repetitions):
+                graph.reset_tau()
+                ant_colony = AntColony(graph, num_ants, num_iterations, colorList)
+                ant_colony.start(colorList)
+                if ant_colony.best_path_cost < best_path_cost:
+                    best_path_vec = ant_colony.best_path_vec
+                    best_path_cost = ant_colony.best_path_cost
+
+            best_path_vec = [x+1 for x in best_path_vec]
+            
+            print "-----------AntColony Result-------------"
+            print "best path = ", best_path_vec
+            print "best path cost is", best_path_cost
+        
+        except Exception as e:
+            print "exception: ", str(e)
+            traceback.print_exc()
+    fout.close()
+    os.chdir(os.path.expanduser("../"))
+    """
---
+++ dptsp/dptsp.py
@@ -0,0 +1,130 @@
+from itertools import combinations
+
+def cci(city):
+    if colors[city] == 'R':
+        return -1
+    else:
+        return 1
+
+def dp(num_cities, distances, colors):
+    def cci(city):
+        if colors[city] == 'R':
+            return -1
+        else:
+            return 1    
+    C={}
+    cities = range(num_cities)
+    for city in cities:
+        C[(frozenset([city]), city, cci(city))] = 0
+
+    s = 2
+    while s <= num_cities:
+        subsets = list(combinations(cities, s))
+        for salami in subsets:
+            salami = list(salami)
+            pastrami = frozenset(salami) 
+            for j in salami:
+                l_subset = salami[:]
+                l_subset.remove(j)
+                f_subset = frozenset(l_subset)
+                top = float('inf')
+                prev_color = None
+                cur_color = cci(j)
+                for v in f_subset:
+                    for i in [-3, -2, -1, 1, 2, 3]:
+                        if (f_subset, v, i) in C and cur_color + i not in [-4, 4]:
+                            comparison = C[(f_subset, v, i)] + distances[v][j]
+                            if top > comparison:
+                                top = comparison
+                                prev_color = i
+                if prev_color:
+                    if cur_color * prev_color > 0:
+                        C[(pastrami, j, prev_color + cur_color)] = top
+                    else:
+                        C[(pastrami, j, cur_color)] = top 
+        s += 1
+    
+    rondo = []
+    s_cities = frozenset(cities)
+    roc = float('inf')
+    next_color = None
+    prev = None
+    for j in cities:
+        for i in [-3, -2, -1, 1, 2, 3]:
+            if (s_cities, j, i) in C:
+                comparison = C[(s_cities, j, i)]
+                if roc >= comparison:
+                    roc = comparison
+                    next_color = i
+                    prev = j
+    rondo.append(prev)
+    cities.remove(prev)
+    dirk = len(cities)
+    q = 0
+    while q < dirk:
+        s_cities = frozenset(cities)
+        top = float('inf')
+        nop = None
+        cor = 0
+        vrep = 0
+        if next_color > 1:
+            io = [next_color - 1]
+        elif next_color == 1:
+            io = [-3, -2, -1]
+        elif next_color < -1:
+            io = [next_color + 1]
+        elif next_color == -1:
+            io = [3, 2, 1]
+        else:
+            print 'color error'
+        for j in cities:
+            for i in io: 
+                if (s_cities, j, i) in C:
+                    if C[(s_cities, j, i)] + distances[j][prev] == roc:
+                        vrep = j
+                        nop = i
+                        cor = C[(s_cities, j, i)]
+        roc = cor
+        prev = vrep
+        next_color = nop
+        rondo.append(prev)
+        cities.remove(prev)
+        q += 1
+    
+    del rondo[-1]
+
+    print 'Dynamic Programming Algorithm'
+    print '============================='
+
+    wat = 0
+    for i in range(len(rondo)-1):
+        print rondo[i]+1, '->', rondo[i+1]+1, ':', distances[rondo[i]][rondo[i+1]]
+        wat += distances[rondo[i]][rondo[i+1]]
+
+    donda = []
+    for r in rondo:
+        donda.append(colors[r])
+
+    for t in range(len(rondo)):
+        rondo[t] += 1
+
+    print '============================='
+    print 'Order:', rondo
+    print 'Colors:', donda
+    print 'Length:', wat
+
+
+if __name__=="__main__":
+    T = 1 # number of test cases                                                    
+    for t in xrange(1, T+1):
+        fin = open(str(t) + ".in", "r")
+        N = int(fin.readline())
+        d = [[] for i in range(N)]
+        for i in xrange(N):
+            d[i] = [int(x) for x in fin.readline().split()]
+        c = fin.readline()
+    num_cities = N
+    distances = d
+    colors = c
+    dp(N, d, c)
+    
